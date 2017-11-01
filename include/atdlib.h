0ee6ea2ad1222d7a71eb165a1c99d0dc7a6d7079
Andrew DeSantis <atd@bitcoin.sh>
2017-11-01 17:21:35 -0500

    update: .gitignore,main.c,include/atdlib.h

--- include/atdlib.h
+++ include/atdlib.h
@@ -1,19 +1,27 @@
 #ifndef __ATDLIB__
 #define __ATDLIB__
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "crypto/aes.h"
 #include "bitcoin/addr.h"
 #include "bitcoin/script.h"
+
 #include "deosaddr.h"
 #include "deosocl.h"
 #include "deostx.h"
 #include "deosutil.h"
+
 #ifdef __cplusplus
 }
 #endif
+
 #endif
